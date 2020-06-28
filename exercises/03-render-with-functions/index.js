9ec6982320a0010b9a23ebcd3d1756d3c5d59231
Abel Segui <Abelsegui@hotmail.com>
2020-06-28 02:43:19 +0000

    3.2done

--- exercises/03-render-with-functions/index.js
+++ exercises/03-render-with-functions/index.js
@@ -3,8 +3,9 @@ import React from "react"; //Main React.js library
 import ReactDOM from "react-dom"; //we use ReactDOM to render into the DOM
 
 // This function returns a string that will be rendered
+var newPrint = "I Love React";
 export const PrintHello = () => {
-	return <h1>Hello World</h1>;
+	return <h1>{newPrint}</h1>;
 };
 
 //              what           where
